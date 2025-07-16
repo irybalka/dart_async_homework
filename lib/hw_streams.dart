3d43057502e3793cd37726661535999d44257fbd
Igor Rybalka <irybalka@yahoo.com>
2025-07-17 00:22:46 +0300

    feat(DART-11-homework): PART II, Task 6 added

--- lib/hw_streams.dart
+++ lib/hw_streams.dart
@@ -1,6 +1,17 @@
-void main() {
-  //
-  int calculate() {
-    return 6 * 7;
+import 'dart:async';
+
+void main() async {
+  final stream = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
+
+  print('Вивід через await for:');
+  await for (final value in stream) {
+    print(value);
   }
+
+  final stream2 = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
+
+  print('Вивід через listen:');
+  stream2.listen((value) {
+    print(value);
+  });
 }
