ac436533d7ea754f592192d8d3c7e7b26d4a10af
Igor Rybalka <irybalka@yahoo.com>
2025-07-16 22:56:10 +0300

    feat(Dart-11-homework): Part 1, task 2 added

--- lib/hw_futures.dart
+++ lib/hw_futures.dart
@@ -1,11 +1,27 @@
 import 'dart:async';
 
+void main() async {
+  print('---------------------- PART I ----------------------------');
+  print('---------------------- Task 1 ----------------------------');
+  String name = await fetchName();
+  print('My name is $name');
+  print('---------------------- Task 2 ----------------------------');
+  String ageString = await fetchAge(33);
+  print(ageString);
+}
+
 Future<String> fetchName() async {
   await Future.delayed(Duration(seconds: 2));
   return 'Ihor';
 }
 
-void main() async {
-  String name = await fetchName();
-  print('My name is $name');
+Future<String> fetchAge(int age) async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  String yearsString = 'років';
+  int lastDigit = age % 10;
+  if (lastDigit == 1) yearsString = 'рік';
+  if ([2, 3, 4].contains(lastDigit)) yearsString = 'роки';
+
+  final resultString = 'мені $age $yearsString';
+  return resultString;
 }
