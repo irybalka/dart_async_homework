29e621c07f5556b11454ec1fc8c9f7712642a7aa
Igor Rybalka <irybalka@yahoo.com>
2025-07-16 13:19:07 +0300

    feat(DART-11-homework): Part 1, Task 1

--- bin/dart_async_homework.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_async_homework/dart_async_homework.dart' as dart_async_homework;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_async_homework.calculate()}!');
-}
