7e70af6975a686b39459ad318539a193c8647d6f
Igor Rybalka <irybalka@yahoo.com>
2025-07-17 00:50:46 +0300

    feat(DART-11-homework): PART II Task 7 added

--- lib/hw_streams.dart
+++ lib/hw_streams.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 void main() async {
+  print('-------------------------- Task 6 ----------------------------------');
   final stream = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
 
   print('Вивід через await for:');
@@ -9,9 +10,22 @@ void main() async {
   }
 
   final stream2 = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
-
   print('Вивід через listen:');
-  stream2.listen((value) {
-    print(value);
-  });
+  final completer = Completer();
+  stream2.listen(
+    (value) => print(value),
+    onDone: () => completer.complete(),
+  );
+  await completer.future;
+  print('-------------------------- Task 7 ----------------------------------');
+
+  final periodicStream = Stream<int>.periodic(
+    Duration(seconds: 1),
+    (count) => count + 1,
+  ).take(10);
+
+  print('Вивід через Stream.periodic:');
+  await for (final value in periodicStream) {
+    print('\u001b[31m$value...\u001b[0m');
+  }
 }
